98b26ad17334dcae63451aabcec924543e472aeb
Sharjeel Junaid <sharjeelj@outlook.com>
2023-02-24 23:57:58 -0700

    Update Database.java & File.java & User.java to all be Record classes - Note this feature features Java SE 14 or higher

--- app/src/main/java/Tracker/Infrastructure/ToyDatabaseServer/Model/File.java
+++ app/src/main/java/Tracker/Infrastructure/ToyDatabaseServer/Model/File.java
@@ -2,37 +2,4 @@ package Tracker.Infrastructure.ToyDatabaseServer.Model;
 
 import java.util.ArrayList;
 
-public class File {
-    private final String filename;
-    private final String hash;
-    private final long size;
-    private ArrayList<String> owners;
-
-    public File(String filename, String hash, long size, ArrayList<String> owners) {
-        this.filename = filename;
-        this.hash = hash;
-        this.size = size;
-        this.owners = owners;
-    }
-
-    public String getFilename() {
-        return this.filename;
-    }
-
-    public String getHash() {
-        return this.hash;
-    }
-
-    public long getSize() {
-        return this.size;
-    }
-
-    public ArrayList<String> getOwners() {
-        return this.owners;
-    }
-
-
-    public void setOwners(ArrayList<String> owners) {
-        this.owners = owners;
-    }
-}
+public record File(String filename, String hash, long size, ArrayList<User> owners) {}
